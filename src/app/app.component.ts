e31c6afb9dbfa84c0737928c72e4a5ef81da5e77
Hans Sebastian Pöhlmann <h.sebastian.p@gmail.com>
2024-07-09 11:38:35 +0200

    file list mockup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,5 @@ import { RouterOutlet } from "@angular/router";
 })
 export class AppComponent{
   title = "shortrest";
+  files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 }
